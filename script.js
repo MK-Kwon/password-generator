730d9902b0a43d4e45e0f9cbb64f1ca48b56bb95
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-06-05 17:24:26 +0930

    Assigned a button id, added variables of four criteria

---
+++ script.js
@@ -0,0 +1,36 @@
+// Assign button id 
+var btnMake = document.getElementById("btnMake");
+
+// Create function to generare password
+function passwordMake () {
+
+  // Create arrays of four criteria 
+  var upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var numArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var characterArr = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+
+  
+  
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
